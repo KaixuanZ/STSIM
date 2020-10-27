9d0479f246ad0cf5fd394348c3a5d0e219173c55
kaixuan zhang <kaixuanzhang2019@u.northwestern.edu>
2020-10-27 18:20:29 -0400

    Dataset() returns all label versions (3 versions with our data)

--- utils/dataset.py
+++ utils/dataset.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 import pandas as pd
 import os
 import torch
@@ -6,7 +7,7 @@ import torch
 clean_names = lambda x: [i for i in x if i[0] != '.']
 
 class Dataset():
-    def __init__(self, image_dir, label_file, device, label_id=2):
+    def __init__(self, image_dir, label_file, device):
         '''
         :param image_dir:
         :param label_file:
@@ -15,25 +16,24 @@ class Dataset():
         '''
         self.image_paths = [os.path.join(image_dir,path) for path in sorted(clean_names(os.listdir(image_dir)))]
         self.label_file = label_file
-        self.labels = self.getlabels(label_id)
+        self.labels = self.getlabels()  # [i,j,k]
         self.N = len(self.image_paths)
         self.cur = 0
         self.device = device
 
 
-    def getlabels(self, label_id):
+    def getlabels(self):
         '''
         :param label_file:
         :param id: the id of label matrix
-        :return: [i-th distortion, j-th texture]
+        :return: [i-th distortion, j-th texture, k-th version of label]
         '''
         df = pd.read_excel(self.label_file, header=None)
-        if label_id==0:
-            return df.iloc[:9,:10].to_numpy()
-        elif label_id==1:
-            return df.iloc[12:21,:10].to_numpy()
-        elif label_id==2:
-            return df.iloc[31:40,:10].to_numpy()
+        label1 = df.iloc[:9,:10].to_numpy().astype(np.double)
+        label2 = df.iloc[12:21,:10].to_numpy().astype(np.double)
+        label3 = df.iloc[31:40,:10].to_numpy().astype(np.double)
+        return np.stack([label1,label2,label3], axis=2)
+
 
     def _getdata(self, pair = True):
         '''
@@ -46,7 +46,6 @@ class Dataset():
         label = self.labels[j, i]
 
         self.cur += 1
-
         if pair:
             H, _ = img.shape
             img1 = img[:H // 2, :]
@@ -69,6 +68,7 @@ class Dataset():
             else:
                 img1, img2, label = self._getdata()
                 X2.append(img2)
+
             X1.append(img1)
             Y.append(label)
             if self.cur == self.N:
