c71d1bb15c04276763b5e5fb6802324bae3d523e
kaixuan zhang <kaixuanzhang2019@u.northwestern.edu>
2020-10-27 16:06:19 -0400

    testing STSIM-1&2 with Borda's rule

--- main.py
+++ main.py
@@ -6,6 +6,22 @@ from metrics.STSIM import Metric
 
 import torch
 
+def Borda_rule(pred, label, N):
+    '''
+    expectation of Person's corr over all textures
+    :param pred: values predicted by the metric
+    :param label: ground truth label
+    :param N: number of distortions per texture
+    :return: Person's corr with Borda's rule
+    '''
+    coeff = 0
+    pred = pred.reshape([N,-1]).cpu().numpy()
+    label = label.reshape([N,-1]).cpu().numpy()
+    for i in range(pred.shape[1]):
+        corr = np.corrcoef(pred[:,i], label[:,i])[0,1]
+        coeff += np.abs(corr)
+    return coeff/pred.shape[1]
+
 
 def stsim_features():
     image_dir = 'data/scenes_distorted'
@@ -36,13 +52,13 @@ def test_stsim():
 
     m_g = Metric(sp3Filters, device=device)
     pred = m_g.STSIM(X1, X2)
-    corr = np.corrcoef(pred.cpu().numpy(), Y.cpu().numpy())[0, 1]
-    print("STSIM-1:",np.abs(corr))  # 0.924
+    print("STSIM-1 (Borda's rule):",Borda_rule(pred, Y, 9)) # 0.896
 
     pred = m_g.STSIM2(X1, X2)
-    corr = np.corrcoef(pred.cpu().numpy(), Y.cpu().numpy())[0, 1]
-    print("STSIM-2:",np.abs(corr))  # 0.926
+    print("STSIM-2 (Borda's rule):", Borda_rule(pred, Y, 9)) # 0.886
 
+    import pdb;
+    pdb.set_trace()
 
 test_stsim()
 
