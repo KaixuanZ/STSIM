643285800b6222c807b3f976111e3a810ea755aa
kaixuan <kaixuanzhang2019@u.northwestern.edu>
2021-03-11 23:48:39 -0500

    remove unnecessary code

--- train.py
+++ train.py
@@ -100,9 +100,6 @@ if __name__ == '__main__':
                 print('validation iter ' + str(i) + ' :', val)
             if i % checkpoint_interval == 0:    # save weights
                 torch.save(model.state_dict(), os.path.join(config['weights_path'], 'epoch_' + str(i).zfill(4) + '.pt'))
-                import pdb;
-
-                pdb.set_trace()
         import pdb;
 
         pdb.set_trace()
