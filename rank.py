aef8ba3a1d5458ec51aa0ec969112d6f5bd153bc
kaixuan <kaixuanzhang2019@u.northwestern.edu>
2021-03-30 11:48:50 -0400

    show how rank effect performance

---
+++ rank.py
@@ -0,0 +1,62 @@
+
+r = []
+res = []
+r.append(82)
+res_tmp = []
+res_tmp.append(972,974,927)
+res_tmp.append(975,978,938)
+res_tmp.append(977,973,922)
+
+r.append(70)
+res_tmp = []
+res_tmp.append(972,972,927)
+res_tmp.append(970,978,938)
+res_tmp.append(978,975,927)
+
+r.append(60)
+res_tmp = []
+res_tmp.append(968,975,927)
+res_tmp.append(977,977,933)
+res_tmp.append(976,976,933)
+
+r.append(50)
+res_tmp = []
+res_tmp.append(970,977,933)
+res_tmp.append(971,978,938)
+res_tmp.append(969,978,938)
+
+r.append(40)
+res_tmp = []
+res_tmp.append(975,979,938)
+res_tmp.append(976,977,938)
+res_tmp.append(974,976,933)
+
+r.append(30)
+res_tmp = []
+res_tmp.append(972,979,938)
+res_tmp.append(971,977,938)
+res_tmp.append(969,977,933)
+
+r.append(20)
+res_tmp = []
+res_tmp.append(973,978,938)
+res_tmp.append(968,972,933)
+res_tmp.append(961,970,927)
+
+r.append(10)
+res_tmp = []
+res_tmp.append(966,973,933)
+res_tmp.append(968,979,944)
+res_tmp.append(969,976,933)
+
+r.append(5)
+res_tmp = []
+res_tmp.append(972,972,933)
+res_tmp.append(969,975,933)
+res_tmp.append(972,978,938)
+
+r.append(1)
+res_tmp = []
+res_tmp.append(962,975,927)
+res_tmp.append(961,965,916)
+res_tmp.append(958,958,905)
