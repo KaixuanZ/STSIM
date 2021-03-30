0fdedc7eb9942beca0c644fbac1dbe7b3cad1a8a
kaixuan <kaixuanzhang2019@u.northwestern.edu>
2021-03-30 16:32:37 -0400

    support torch complex value

--- filterbank/math_utils.py
+++ filterbank/math_utils.py
@@ -30,21 +30,17 @@ def roll_n(X, axis, n):
     return torch.cat([back, front], axis)
 
 def batch_fftshift2d(x):
-    real, imag = torch.unbind(x, -1)
-    for dim in range(1, len(real.size())):
-        n_shift = real.size(dim)//2
-        if real.size(dim) % 2 != 0:
+    for dim in range(1, len(x.size())):
+        n_shift = x.size(dim)//2
+        if x.size(dim) % 2 != 0:
             n_shift += 1  # for odd-sized images
-        real = roll_n(real, axis=dim, n=n_shift)
-        imag = roll_n(imag, axis=dim, n=n_shift)
-    return torch.stack((real, imag), -1)  # last dim=2 (real&imag)
+        x = roll_n(x, axis=dim, n=n_shift)
+    return x
 
 def batch_ifftshift2d(x):
-    real, imag = torch.unbind(x, -1)
-    for dim in range(len(real.size()) - 1, 0, -1):
-        real = roll_n(real, axis=dim, n=real.size(dim)//2)
-        imag = roll_n(imag, axis=dim, n=imag.size(dim)//2)
-    return torch.stack((real, imag), -1)  # last dim=2 (real&imag)
+    for dim in range(len(x.size()) - 1, 0, -1):
+        x = roll_n(x, axis=dim, n=x.size(dim)//2)
+    return x
 
 ################################################################################
 ################################################################################
