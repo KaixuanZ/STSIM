60944f021394a97415f3fab96b5ff87754715e81
Kaixuan <kaixuanzhang2019@u.northwestern.edu>
2021-12-01 23:18:03 -0600

    plot PSD of radius average and y axis log scale

--- PSD.py
+++ PSD.py
@@ -13,7 +13,7 @@ from metrics.STSIM import *
 import torch
 from scipy.stats import wasserstein_distance
 
-def _GetPSD1D(psd2D, average=False):
+def _GetPSD1D(psd2D, average=True):
     h  = psd2D.shape[0]
     w  = psd2D.shape[1]
     wc = w//2
@@ -31,7 +31,7 @@ def _GetPSD1D(psd2D, average=False):
         # SUM all psd2D pixels with label 'r' for 0<=r<=wc
         # NOTE: this will miss power contributions in 'corners' r>wc
         psd1D = ndimage.sum(psd2D, r, index=np.arange(0, min(wc,hc)))
-
+    psd1D[0] = 0
     return psd1D
 
 def GetPSD1D(img):
@@ -52,11 +52,11 @@ def main(img_path1, img_path2, label, psd1d_o, viz=True, prefix=""):
 
     fg = img1 - img2
     #fg = fg[h:h + size, w:w + size]
-    t = min(-fg.min(), fg.max())
+    fg = fg[:900, -900:]
     cv2.imwrite('_'.join([prefix,'tmp.png']), (fg+t)*255/2/t)
 
-    fg = fg-fg.mean()
     psd1d_fg = GetPSD1D(fg)
+    psd1d_fg[0] = 0
     # Gaussian = np.random.normal(0, (fg ** 2).mean() ** 0.5, fg.shape)
     # psd1d_Gaussian = GetPSD1D(Gaussian)
     if viz:
@@ -80,11 +80,11 @@ def main(img_path1, img_path2, label, psd1d_o, viz=True, prefix=""):
 def main1(img_path1, label, psd1d_o, viz=True):
     # for H&B synthesis
     fg = cv2.imread(img_path1, 0).astype(float)
-    fg = fg/255*28-14
+    fg = fg/255*t*2 - t
 
     h,w = fg.shape
     #fg = fg[h//4:-h//4, w//4:-w//4]
-    fg = fg[:1080, :1080]
+    fg = fg[:900, -900:]
     #t = min(-fg.min(), fg.max())
     #cv2.imwrite('tmp.png', (fg+t)*255/2/t)
     psd1d_fg = GetPSD1D(fg)
@@ -115,7 +115,7 @@ def Wasserstein_dis(psd1, psd2, viz=True, figname='tmp1.png', normalize=True, ST
         w_dis = wasserstein_distance(psd1, psd2)
 
     if viz:
-        k = 0
+        k = 1
         plt.plot(range(k,psd1.size), psd1[k:], label='original')
         plt.plot(range(k,psd2.size), psd2[k:], label='synthesized')
         plt.legend()
@@ -124,57 +124,42 @@ def Wasserstein_dis(psd1, psd2, viz=True, figname='tmp1.png', normalize=True, ST
         else:
             plt.title('w_dist={:0.4f}, STSIM-Mf={:0.4f}'.format(w_dis,STSIM_Mf))
         #plt.show()
+        plt.yscale('log')
         plt.savefig(figname)
         plt.close()
     return w_dis
 
+def plot_psds(psd1, psd2, psd3, figname='tmp1.png', normalize=True):
+    if normalize:
+        w_dis1 = wasserstein_distance(psd1 / np.sum(psd1), psd2 / np.sum(psd2))
+        w_dis2 = wasserstein_distance(psd1 / np.sum(psd1), psd3 / np.sum(psd3))
+    else:
+        w_dis1 = wasserstein_distance(psd1, psd2)
+        w_dis2 = wasserstein_distance(psd1, psd3)
 
-img_o = '/Data/DareDevil/denoised/frame_00300.png'
-img_den = '/Data/DareDevil/original/frame_00300.png'
+    k = 1
+    plt.plot(range(k,psd1.size), psd1[k:], label='original')
+    plt.plot(range(k,psd2.size), psd2[k:], label='av1_synthesized')
+    plt.plot(range(k,psd3.size), psd3[k:], label='H&B_synthesized')
+    plt.legend()
+    plt.title('wasserstein distance={:0.5f}, {:0.5f}'.format(w_dis1, w_dis2))
+    #plt.show()
+    plt.yscale('log')
+    plt.savefig(figname)
+    plt.close()
+
+t = 6
+
+img_den = 'data/brooklyn_nine_nine/denoised/frame_00001.png'
+img_o = 'data/brooklyn_nine_nine/original/frame_00001.png'
 psd1d_o = main(img_o, img_den, 'original film grain', psd1d_o=None, viz=False)
 
-#fg = '/Data/DareDevil/synthesized_H&B/frame_00300.png'
-# fg = '/Data/DareDevil/synthesized_H&B/fg_synthesized_scaled.png'
-# psd1d_syn_HB = main1(fg, 'synthesized film grain H&B', psd1d_o)
+img_dec = 'data/brooklyn_nine_nine/decoded/frame_00001.png'
+img_ren = 'data/brooklyn_nine_nine/renoised/frame_00001.png'
+psd1d_syn_av1 = main(img_ren, img_dec, 'av1 synthesized film grain', psd1d_o=None, viz=False)
 
-# print(Wasserstein_dis(psd1d_o, psd1d_syn_HB, figname='tmp6.png', STSIM_Mf=None))
+fg = 'data/brooklyn_nine_nine/fg_synthesized_scaled.png'
+psd1d_syn_HB = main1(fg, 'H&B synthesized film grain', psd1d_o, viz=False)
 
-
-# img_dec = '/Data/DareDevil/decoded/frame_00300.png'
-# img_ren = '/Data/DareDevil/renoised_default/frame_00300.png'
-# psd1d_syn0 = main(img_dec, img_ren, 'synthesized film grain', psd1d_o=psd1d_o, prefix='tmp/syn0')
-#
-# img_dec = '/Data/DareDevil/decoded/frame_00300.png'
-# img_ren = '/Data/DareDevil/renoised_01/frame_00300.png'
-# psd1d_syn1 = main(img_dec, img_ren, 'synthesized film grain', psd1d_o=psd1d_o, prefix='tmp/syn1')
-#
-# img_dec = '/Data/DareDevil/decoded/frame_00300.png'
-# img_ren = '/Data/DareDevil/renoised_02/frame_00300.png'
-# psd1d_syn2 = main(img_dec, img_ren, 'synthesized film grain', psd1d_o=psd1d_o, prefix='tmp/syn2')
-#
-# img_dec = '/Data/DareDevil/decoded/frame_00300.png'
-# img_ren = '/Data/DareDevil/renoised_03/frame_00300.png'
-# psd1d_syn3 = main(img_dec, img_ren, 'synthesized film grain', psd1d_o=psd1d_o, prefix='tmp/syn3')
-#
-# img_dec = '/Data/DareDevil/decoded/frame_00300.png'
-# img_ren = '/Data/DareDevil/renoised_04/frame_00300.png'
-# psd1d_syn4 = main(img_dec, img_ren, 'synthesized film grain', psd1d_o=psd1d_o, prefix='tmp/syn4')
-
-img_dec = '/Data/DareDevil/decoded/frame_00300.png'
-img_ren = '/Data/DareDevil/renoised_default_ps64/frame_00300.png'
-psd1d_syn5 = main(img_dec, img_ren, 'synthesized film grain', psd1d_o=psd1d_o, prefix='tmp/syn5')
-
-# print(Wasserstein_dis(psd1d_o, psd1d_syn0, figname='tmp1.png', STSIM_Mf=0.1678))
-# print(Wasserstein_dis(psd1d_o, psd1d_syn1, figname='tmp2.png', STSIM_Mf=0.2503))
-# print(Wasserstein_dis(psd1d_o, psd1d_syn2, figname='tmp3.png', STSIM_Mf=0.2321))
-# print(Wasserstein_dis(psd1d_o, psd1d_syn3, figname='tmp4.png', STSIM_Mf=0.2319))
-# print(Wasserstein_dis(psd1d_o, psd1d_syn4, figname='tmp5.png', STSIM_Mf=0.1810))
-#
-# img_dec = '/Data/DareDevil/decoded/frame_00300.png'
-# img_ren = '/Data/DareDevil/renoised_Daizong/frame300_averaged_binary_n64r4v0.tif'
-# psd1d_syn2 = main(img_dec, img_ren, 'synthesized film grain n64r4v0')
-#
-# img_o = '/Data/DareDevil/denoised/frame_00300.png'
-# img_den = '/Data/DareDevil/renoised_Daizong/frame300_denoised_averaged_binary_n96r4v0.tif'
-# psd1d_syn3 = main(img_o, img_den, 'synthesized film grain n96r4v0')
-#
+# print(Wasserstein_dis(psd1d_o, psd1d_syn_HB, figname='tmp1.png', STSIM_Mf=None))
+plot_psds(psd1d_o, psd1d_syn_av1, psd1d_syn_HB, figname='tmp1.png')
